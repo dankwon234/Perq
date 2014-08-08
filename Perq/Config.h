6dd0a9300f13b019df15ea218adee3229f61fb7e
Denny Kwon <info@tsunami234.com>
2014-08-08 15:20:28 -0400

    added color constants

--- Perq/Config.h
+++ Perq/Config.h
@@ -12,4 +12,8 @@
 
 #define kPostCellDimension 130.0f
 
+// Colors:
+#define kRed [UIColor colorWithRed:192.0f/255.0f green:35.0f/255.0f blue:59.0f/255.0f alpha:1.0f]
+#define kGreen [UIColor colorWithRed:62.0f/255.0f green:115.0f/255.0f blue:132.0f/255.0f alpha:1.0f]
+
 #endif
