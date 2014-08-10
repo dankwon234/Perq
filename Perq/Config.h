2ea9af21f4c9e88211660bc7bba9b950f926ec46
Denny Kwon <info@tsunami234.com>
2014-08-09 20:18:26 -0400

    added social apps constants

--- Perq/Config.h
+++ Perq/Config.h
@@ -16,4 +16,12 @@
 #define kRed [UIColor colorWithRed:192.0f/255.0f green:35.0f/255.0f blue:59.0f/255.0f alpha:1.0f]
 #define kGreen [UIColor colorWithRed:62.0f/255.0f green:115.0f/255.0f blue:132.0f/255.0f alpha:1.0f]
 
+// Social:
+#define kTwitterAPI @"https://api.twitter.com/1.1/"
+#define kFacebookAPI @"https://graph.facebook.com/v2.1/"
+#define kFacebookAppID @"634876436619394"
+#define kFacebookAppSecret @"08b33bfae94013f2c72b68d8c2b618d6"
+#define kFacebookPermissions @[@"email", @"user_likes"]
+
+
 #endif
